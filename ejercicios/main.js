ddfe3f90e157593330f254397bb97f4b24e538d4
Williams Hernández <hernandw@gmail.com>
2023-08-17 22:54:57 -0400

    :tada: ejercicios hecho en clase de api

---
+++ ejercicios/main.js
@@ -0,0 +1,25 @@
+const numeros1 = [1, 2, 3 , 4 , 5, 6, 7]
+
+
+
+/* numeros1.forEach((n) => {
+    console.log(n)
+    return n
+    
+}) */
+
+
+/* const mostrarNumeros = numeros1.forEach((n) => {
+    console.log(n)
+    return n
+}) */
+
+/* console.log(mostrarNumeros) */
+
+
+const mostrarNumerosconMap = numeros1.map((n) => {
+    console.log(n)
+return n
+})
+
+console.log(mostrarNumerosconMap)
